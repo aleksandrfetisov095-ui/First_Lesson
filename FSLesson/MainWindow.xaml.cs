f6ad4753be3daacb3b87cb71c72edeaeea17ca69
Fetis_ep98@top-academy.ru <Fetis_ep98@top-academy.ru>
2026-01-26 20:16:06 +0300

    дзffds

---
+++ FSLesson/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FSLesson
+{
+    public partial class MainWindow : Window
+    {
+        
+        private FirstExample.SumW sumForm;
+        private SecondExample.DiffW differenceForm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenSumFormButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sumForm == null || sumForm.IsVisible)
+            {
+                sumForm = new FirstExample.SumW();  
+                sumForm.Owner = this;
+                sumForm.Show();
+            }
+            else
+            {
+                sumForm.Activate();
+            }
+        }
+
+        private void OpenDifferenceFormButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (differenceForm == null || !differenceForm.IsVisible)
+            {
+                differenceForm = new SecondExample.DiffW();  
+                differenceForm.Owner = this;
+                differenceForm.Show();
+            }
+            else
+            {
+                differenceForm.Activate();
+            }
+        }
+
+        private void OpenBothFormsButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenSumFormButton_Click(sender, e);
+            OpenDifferenceFormButton_Click(sender, e);
+        }
+    }
+}
---
+++ SecondExample/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SecondExample
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class DiffW : Window
+    {
+        public DiffW()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверка ввода
+            if (!double.TryParse(InputTextBox.Text, out double number))
+            {
+                MessageBox.Show("Введите число!");
+                return;
+            }
+
+            
+            ProgressBar.Visibility = Visibility.Visible;
+            CalculateButton.IsEnabled = false;
+
+            //  запуск в фоне
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                
+                double result = SimpleDifference(number);
+
+                // 5. возвращение в гл. поток
+                Dispatcher.Invoke(() =>
+                {
+                    ResultTextBox.Text = result.ToString();
+                    ProgressBar.Visibility = Visibility.Collapsed;
+                    CalculateButton.IsEnabled = true;
+                });
+            });
+        }
+
+        private double SimpleDifference(double number)
+        {
+            Thread.Sleep(1000); // 1сек
+
+            
+            double diff = (number * 10) - 3;
+
+            return diff;
+        }
+    }
+}
